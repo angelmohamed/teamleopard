c95d8b8aaeff55b46a02e0306c2f920e401e0254
DannyJT27 <daniel.tierney4@gmail.com>
2025-03-08 21:27:57 +0000

    landing page mobile support + logo

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Link from "next/link"; // Correct import for Next.js Link
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
   Carousel,
@@ -19,28 +20,32 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 {
-  /* THINGS TO ADD: MOBILE SIZING SUPPORT, ACTUAL STAT IMPORT??, MEET THE TEAM SECTION??*/
-}
-{
-  /* THINGS TO CHANGE: COLOUR SCHEME, TEXT BOXES, PLACEHOLDER IMAGES, SELECTION COLOUR FOR TABS (you cant see the selected item well)*/
+  /* THINGS TO CHANGE: COLOUR SCHEME, TEXT BOXES, PLACEHOLDER IMAGES, SELECTION COLOUR FOR TABS (you cant see the selected item well), LAST CARDS SECTION*/
 }
 
 export default function HomePage() {
   return (
     <div className="bg-white">
       {/* NAVBAR ON TOP */}
-      <header className="sticky top-0 z-50 bg-white text-slate-100 shadow-lg">
-        <div className="flex justify-between items-center px-4 py-2">
-          <h1 className="text-xl text-black font-bold">Connect</h1>
+      <header className="sticky top-0 z-50 bg-white text-x1 text-slate-100 shadow-lg">
+        <div className="flex justify-between items-center px-2 py-2">
+          <Image
+            src="/logo.png"
+            alt="Company Logo"
+            width={105}
+            height={40}
+            priority
+            className="ml-1"
+          />
           <nav>
             <ul className="flex space-x-4">
-              <Button className="text-x1" asChild>
-                <Link href="/sign-up">Sign Up</Link>
-              </Button>
-              <Button className="text-x1" asChild>
+              <Button asChild>
                 <Link href="/login">Log In</Link>
               </Button>
-              <Button className="text-x1" asChild>
+              <Button asChild>
+                <Link href="/sign-up">Sign Up</Link>
+              </Button>
+              <Button asChild>
                 <Link href="/company-sign-up">Register As Employer</Link>
               </Button>
             </ul>
@@ -49,30 +54,31 @@ export default function HomePage() {
       </header>
 
       {/* SECTION 1 - START WITH BACKGROUND IMAGE */}
-      <section className="flex justify-center items-center h-[75vh] min-h-[500px] text-white text-4xl font-bold py-6">
+      <section className="flex flex-col md:flex-row justify-center items-center h-[75vh] min-h-[500px] text-white text-4xl font-bold py-6">
         {/* INTRO TEXT AND BUTTON */}
-        <div className="w-1/2 flex flex-col items-center justify-left text-right px-10 gap-6">
-          <h1 className="text-black text-6xl font-bold self-end mr-5">
-            Name of the app or something.
-          </h1>
-          <p className="text-gray-600 text-4x1 italic self-end mr-5">
-            get a job ️‍🔥️‍🔥.
-          </p>
+        <div className="w-full md:w-1/2 flex flex-col items-center justify-center text-center md:text-right gap-6 px-4 md:px-0">
+          <Image
+            src="/logo_big.png"
+            width={420}
+            height={120}
+            layout="responsive"
+            className="w-[90%] max-w-[420px] h-auto"
+          />
           <Button
-            className="bg-slate-700 text-2xl self-end mr-5 bold italic"
+            className="bg-slate-700 text-xl md:text-3xl self-center bold italic"
             asChild
           >
-            <Link href="/sign-up">Start now →</Link>
+            <Link href="/sign-up">Search now →</Link>
           </Button>
         </div>
 
-        <div className="w-1/3 flex items-center justify-right">
+        <div className="w-full md:w-1/2 flex items-center justify-center overflow-hidden mt-6 md:mt-0">
           {/* old code with dynamic image (scrapped for carousel but kept here just incase)
           <img src="https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg" alt="Example" 
           className="w-3/4 rounded-lg shadow-lg transform transition duration-300 ease-in-out hover:scale-105 hover:rotate-1 self-start mr-5" />*/}
 
           {/* CAROUSEL(shadcnui) TO DISPLAY IMAGES OF APP */}
-          <Carousel opts={{ loop: true }}>
+          <Carousel opts={{ loop: true }} className="w-[90%] md:w-[80%]">
             <CarouselContent>
               {/* LIST OF IMAGES OF THE APP */}
               {/* TBC - make sure all images are the same proportions. the carousell will adjust based on the largest image's dimensions. */}
@@ -80,30 +86,29 @@ export default function HomePage() {
                 <img
                   src="https://media.gettyimages.com/id/1410538853/photo/young-man-in-the-public-park.jpg?s=612x612&w=gi&k=20&c=EpcwID8Bfxufrodoyo_x10JgFcnkPNuShJuJHRZkIlM="
                   alt="Example"
-                  className="rounded-lg shadow-lg"
+                  className="rounded-lg shadow-lg w-full h-auto"
                 ></img>
               </CarouselItem>
               <CarouselItem>
                 <img
                   src="https://media.gettyimages.com/id/1410538853/photo/young-man-in-the-public-park.jpg?s=612x612&w=gi&k=20&c=EpcwID8Bfxufrodoyo_x10JgFcnkPNuShJuJHRZkIlM="
                   alt="Example"
-                  className="rounded-lg shadow-lg"
+                  className="rounded-lg shadow-lg w-full h-auto"
                 ></img>
               </CarouselItem>
               <CarouselItem>
                 <img
                   src="https://media.gettyimages.com/id/1410538853/photo/young-man-in-the-public-park.jpg?s=612x612&w=gi&k=20&c=EpcwID8Bfxufrodoyo_x10JgFcnkPNuShJuJHRZkIlM="
                   alt="Example"
-                  className="rounded-lg shadow-lg"
+                  className="rounded-lg shadow-lg w-full h-auto"
                 ></img>
               </CarouselItem>
             </CarouselContent>
-            <CarouselPrevious className="bg-slate-500" />
-            <CarouselNext className="bg-slate-500" />
+            <CarouselPrevious className="bg-slate-500 absolute left-2" />
+            <CarouselNext className="bg-slate-500 absolute right-2" />
           </Carousel>
         </div>
-        {/* empty div to center the rest (this likely has alternatives that I am unaware of)*/}
-        <div className="w-1/6"></div>
+
       </section>
 
       {/* SECTION 2 - EXPLANATION OF PRODUCT SPLIT WITH TABS(shadcnui) */}
@@ -111,28 +116,28 @@ export default function HomePage() {
         <Tabs defaultValue="personal" className="bg-slate-100">
           <TabsList className="w-full">
             <TabsTrigger value="personal" className="w-1/2 bg-slate-100">
-              <h1 className="text-slate-800 text-2xl italic font-bold self-end mr-5">
-                Job Hunters (might need a better word)
+              <h1 className="text-slate-800 text-xl md:text-2xl  italic font-bold self-end mr-5">
+                Job Seekers
               </h1>
             </TabsTrigger>
             <TabsTrigger value="business" className="w-1/2 bg-slate-100">
-              <h1 className="text-slate-800 text-2xl italic font-bold self-end mr-5">
+              <h1 className="text-slate-800 text-xl md:text-2xl italic font-bold self-end mr-5">
                 Businesses
               </h1>
             </TabsTrigger>
           </TabsList>
-          <TabsContent value="personal">
+          <TabsContent value="personal" className="w-full flex justify-center">
             <section
               className="w-screen bg-slate-200 flex justify-center items-center h-[75vh] min-h-[500px] text-white text-4xl font-bold py-6"
               style={{
                 backgroundImage:
-                  "url(https://media.gettyimages.com/id/1410538853/photo/young-man-in-the-public-park.jpg?s=612x612&w=gi&k=20&c=EpcwID8Bfxufrodoyo_x10JgFcnkPNuShJuJHRZkIlM=)",
+                  "url(/landing_page_jobseekers_bg.png)",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
             >
-              {/* Message for Job Hunters */}
-              <Card className="bg-slate-200">
+              {/* Message for Job Seekers */}
+              <Card className="bg-slate-200 w-[90%] max-w-[600px] mx-auto">
                 <CardHeader>
                   <CardTitle>
                     <b>Searching for a career perfect for You?</b>
@@ -155,18 +160,18 @@ export default function HomePage() {
               </Card>
             </section>
           </TabsContent>
-          <TabsContent value="business">
+          <TabsContent value="business" className="w-full flex justify-center">
             <section
               className="w-screen bg-slate-300 flex justify-center items-center h-[75vh] min-h-[500px] text-white text-4xl font-bold py-6"
               style={{
                 backgroundImage:
-                  "url(https://media.istockphoto.com/id/1696781145/photo/modern-building-in-the-city-with-blue-sky.jpg?s=612x612&w=0&k=20&c=POfayTyDe06tGX4CeJgS8-fb896MUC46dl3ZbHXBqN4=)",
+                  "url(/landing_page_businesses_bg.png)",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
             >
               {/* Message for Employers */}
-              <Card className="bg-slate-100">
+              <Card className="bg-slate-100 w-[90%] max-w-[600px] mx-auto">
                 <CardHeader>
                   <CardTitle>
                     <b>Looking to hire the best for your Business?</b>
@@ -192,13 +197,115 @@ export default function HomePage() {
         </Tabs>
       </div>
 
-      {/* SECTION 3 - WHY CHOOSE US + STATS */}
-      <section className="bg-white flex justify-center items-center h-[75vh] min-h-[500px] text-black text-4xl font-bold py-6">
-        WIP
+      {/* SECTION 3 - USER REVIEWS AS A CAROUSEL OF CARDS */}
+      <section className="bg-slate-300 flex flex-col justify-between items-center w-full h-[75vh] min-h-[500px] text-black text-4xl py-6">
+        <div className="text-center text-2xl">
+          What our users think...
+        </div>
+        {/* CAROUSEL */}
+        <div className="flex-grow flex justify-center items-center w-[90%]">
+          <Carousel opts={{ loop: true }} className="w-[90%] md:w-[80%]">
+            <CarouselContent>
+              <CarouselItem className="basis-full md:basis-1/3">
+                <Card className="h-full flex flex-col bg-slate-100">
+                  <CardHeader>★★★★★</CardHeader>
+                  <CardContent className="flex-grow"><p className="text-2xl italic">
+                    "Landed my current job nearly 6 years ago with <b>Connect</b>, still loving it today!"
+                  </p></CardContent>
+                  <CardFooter className="mt-auto"><p className="text-xl">
+                    @james_heff - Job Seeker
+                  </p></CardFooter>
+                 </Card>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/3">
+                <Card className="h-full flex flex-col">
+                  <CardHeader>★★★★★</CardHeader>
+                  <CardContent className="flex-grow"><p className="text-2xl italic">
+                    "Running a small firm had never been easier. Over half of our team crossed paths through <b>Connect</b>.
+                  </p></CardContent>
+                  <CardFooter className="mt-auto"><p className="text-xl">
+                    @jr_clarks - Business Owner
+                  </p></CardFooter>
+                 </Card>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/3">
+                <Card className="h-full flex flex-col bg-slate-100">
+                  <CardHeader>★★★★★</CardHeader>
+                  <CardContent className="flex-grow"><p className="text-2xl italic">
+                    "I went through nearly a dozen jobs with little passion. <b>Connect</b> found me my perfect career within days."
+                  </p></CardContent>
+                  <CardFooter className="mt-auto"><p className="text-xl">
+                    @steverson  - Job Seeker
+                  </p></CardFooter>
+                 </Card>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/3">
+                <Card className="h-full flex flex-col">
+                  <CardHeader>★★★★★</CardHeader>
+                  <CardContent className="flex-grow"><p className="text-2xl italic">
+                    "<b>Connect</b> was able to recommend me my dream position within minutes, and got me the offer within days."
+                  </p></CardContent>
+                  <CardFooter className="mt-auto"><p className="text-xl">
+                    @mariahc - Job Seeker
+                  </p></CardFooter>
+                 </Card>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/3">
+                <Card className="h-full flex flex-col bg-slate-100">
+                  <CardHeader>★★★★★</CardHeader>
+                  <CardContent className="flex-grow"><p className="text-2xl italic">
+                    "Through <b>Connect</b> I've grown my online business faster than I ever could alone.
+                  </p></CardContent>
+                  <CardFooter className="mt-auto"><p className="text-xl">
+                    @leo_j - Business Owner
+                  </p></CardFooter>
+                 </Card>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/3">
+                <Card className="h-full flex flex-col">
+                  <CardHeader>★★★★★</CardHeader>
+                  <CardContent className="flex-grow"><p className="text-2xl italic">
+                    "<b>Connect</b> saved me."
+                  </p></CardContent>
+                  <CardFooter className="mt-auto"><p className="text-xl">
+                    @paul - Job Seeker
+                  </p></CardFooter>
+                 </Card>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/3">
+                <Card className="h-full flex flex-col bg-slate-100">
+                  <CardHeader>★★★★★</CardHeader>
+                  <CardContent className="flex-grow"><p className="text-2xl italic">
+                    "The tools offered by <b>Connect</b> are simply unrivaled."
+                  </p></CardContent>
+                  <CardFooter className="mt-auto"><p className="text-xl">
+                    @marievankane - Job Seeker
+                  </p></CardFooter>
+                 </Card>
+              </CarouselItem>
+              <CarouselItem className="basis-full md:basis-1/3">
+                <Card className="h-full flex flex-col">
+                  <CardHeader>★★★★★</CardHeader>
+                  <CardContent className="flex-grow"><p className="text-2xl italic">
+                    "I wouldn't be where I am today without the help of <b>Connect</b>."
+                  </p></CardContent>
+                  <CardFooter className="mt-auto"><p className="text-xl">
+                    @m_puranam - Job Seeker
+                  </p></CardFooter>
+                 </Card>
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious className="bg-slate-500 absolute -left-10" />
+            <CarouselNext className="bg-slate-500 absolute -right-10" />
+          </Carousel>
+        </div>
+        <div className="text-center text-2xl">
+          Join them today for free.
+        </div>
       </section>
 
       {/* to add a future section
-      <section className="bg-slate-300 flex justify-center items-center h-[75vh] min-h-[500px] text-white text-4xl font-bold py-6">
+      <section className="bg-slate-300 flex justify-center items-center h-[75vh] min-h-[500px] text-white text-4xl py-6">
         CONTENT
       </section>*/}
     </div>
