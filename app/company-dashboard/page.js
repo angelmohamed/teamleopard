81e6273a9e5b1c2d69810bbc26031cd48e7e6dd4
Paul San Diego <johnpaul20030@gmail.com>
2025-03-19 00:38:35 +0000

    minor fix layout

--- app/company-dashboard/page.js
+++ app/company-dashboard/page.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button"; 
+import { Button } from "@/components/ui/button";
 import { Description } from "@radix-ui/react-dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
