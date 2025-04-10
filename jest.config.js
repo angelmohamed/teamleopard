46607fdb9cb52ad86dc76982bb57b68a3fa896c4
DannyJT27 <daniel.tierney4@gmail.com>
2025-04-10 11:20:07 +0100

    IMPORTANT TEST CONFIG FIX

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    testEnvironment: "jsdom",
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-    moduleNameMapper: {
-      "^@/(.*)$": "<rootDir>/$1",
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    },
-    collectCoverage: true,
-    collectCoverageFrom: ["**/*.{js,jsx}", "!**/node_modules/**", "!**/.next/**"],
-    coverageReporters: ["html", "text"],
-    transform: {
-      "^.+\\.[jt]sx?$": "babel-jest"
-    },
-    transformIgnorePatterns: [
-      "/node_modules/(?!next|@babel|lucide-react)"
-    ]
-  };
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.{js,jsx}", "!**/node_modules/**", "!**/.next/**"],
+  coverageReporters: ["html", "text"],
+  transform: {
+    "^.+\\.[jt]sx?$": ["babel-jest", { configFile: "./jest-babel.config.js" }]
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!next|@babel|lucide-react)"
+  ]
+};
